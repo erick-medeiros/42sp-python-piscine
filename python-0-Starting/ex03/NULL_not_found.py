1ac5d94b69ea05293edc730bb2599f730e4d6d0d
Erick Medeiros <erickmedeirosbr@gmail.com>
2025-08-31 16:55:27 -0300

    feat: add python0/ex03

---
+++ python-0-Starting/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+import math
+
+
+def NULL_not_found(object: any) -> int:
+    if isinstance(object, type(None)):
+        print("Nothing: None", type(None))
+    elif isinstance(object, float) and math.isnan(object):
+        print("Cheese: nan", type(object))
+    elif isinstance(object, bool) and object is False:
+        print("Fake: False", type(object))
+    elif isinstance(object, int) and object == 0:
+        print("Zero: 0", type(object))
+    elif isinstance(object, str) and len(object) == 0:
+        print("Empty:", type(object))
+    else:
+        print("Type not found")
+    return 1
---
+++ python-0-Starting/ex03/README.md
@@ -0,0 +1,16 @@
+```sh`
+$>python tester.py | cat -e
+Nothing: None <class 'NoneType'>$
+Cheese: nan <class 'float'>$
+Zero: 0 <class 'int'>$
+Empty: <class 'str'>$
+Fake: False <class 'bool'>$
+Type not Found$
+1$
+$>
+```
+
+```sh
+$>python NULL_not_found.py | cat -e
+$>
+```
