e8d972469e7d2c905a32ab7e95849e83895876b6
Erick Medeiros <erickmedeirosbr@gmail.com>
2025-08-31 17:37:14 -0300

    refactor: improve readability of output in format_ft_time.py

--- python-0-Starting/ex01/format_ft_time.py
+++ python-0-Starting/ex01/format_ft_time.py
@@ -2,8 +2,13 @@ import time
 from datetime import datetime
 
 seconds_epoch = time.time()
+
 print(
-    f"Seconds since January 1, 1970: {seconds_epoch:,.4f} or {seconds_epoch:.2e} in scientific notation"
+    "Seconds since January 1, 1970:",
+    f"{seconds_epoch:,.4f}",
+    "or",
+    f"{seconds_epoch:.2e}",
+    "in scientific notation",
 )
 
 current_date = datetime.now()
