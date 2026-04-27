49030527894bc759ba7e6f3d923f9b261426d28e
Erick Medeiros <erickmedeirosbr@gmail.com>
2026-04-27 20:17:00 -0300

    feat: setup python-1-Array/ex01

---
+++ python-1-Array/ex01/array2D.py
@@ -0,0 +1,20 @@
+def slice_me(family: list, start: int, end: int) -> list:
+    # your code here
+    pass
+
+
+def main():
+    """Test slice_me function."""
+    try:
+        family = [[1.80, 78.4],
+                  [2.15, 102.7],
+                  [2.10, 98.5],
+                  [1.88, 75.2]]
+        print(slice_me(family, 0, 2))
+        print(slice_me(family, 1, -2))
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
